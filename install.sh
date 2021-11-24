e32f431adb44eada6a5f2dd41758e65d7c47cb5d
Said Mavlyan <said.mavlyan@protonmail.com>
2021-11-24 13:57:30 +0600

    update

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ tree
 dict
 htop
 traceroute
+calibre
+xclip
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
